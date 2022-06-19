ff90550b7991351845056c9e1455ca0ecd8c9042
KadsonLima <kadson-kaio@hotmail.com>
2022-06-19 01:33:41 -0300

    feat: integração banco de dados

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
+
 
 const server = express();
 
 server.use(cors(), express.json())
 
-let users = [];
+let database = JSON.parse(fs.readFileSync("database.json", "utf-8"));
+
+let users = database.user;
 let mensagens = [];
 
 server.post('/sign-up', (req, res)=>{
-    if(req.body.username == "" || req.body.avatar == "") res.status(400).send("BADREQUEST");
-    users.push(req.body)
-    res.status(200).send("OK")
+    const {username, avatar} = req.body;
+    if(!username || !avatar) {res.status(400).send("Username e Avatar Vazios !!"); return}
+    if(users.forEach((user)=>{
+        if(user.username == username){
+         res.status(400).send("Username Já existente !!");
+         return;
+        }
+    }));
+
+    users.push(req.body);
+    fs.writeFileSync("database.json", JSON.stringify({user:users}, null , 2));
+    res.status(200).send("OK");
 
 });
 
@@ -22,14 +35,21 @@ server.get('/sign-up', (req, res)=>{
 
 
 server.post('/tweets', (req, res)=>{
-    console.log(req.body)
-    users.forEach(e=>{
-        if(e.username === req.body.username){
-            let msg = req.body;
-            msg['avatar'] = e.avatar;
-            mensagens.push(msg);
-        }
-    })
+    if(req.body.username == "" || req.body.tweet == "") {
+        return res.sendStatus(400);
+    }else{
+        users.forEach(e=>{
+            if(e.username === req.body.username){
+                let msg = req.body;
+                msg['avatar'] = e.avatar;
+                mensagens.push(msg);
+            }
+        })
+    }
+    
+    console.log(req.body);
+    
+    
     res.status(200).send(mensagens);
 
 });
